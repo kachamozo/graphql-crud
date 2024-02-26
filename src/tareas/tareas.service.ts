8326d8261d3d2a407160e7fcccabc838adc82a99
roly ochoa <cachamocito@yahoo.com>
2024-02-26 14:15:33 -0400

    fix tarea service graphql

--- src/tareas/tareas.service.ts
+++ src/tareas/tareas.service.ts
@@ -6,6 +6,7 @@ import { Model, Types } from 'mongoose';
 import { BuscarTareaDto } from './dto/buacar-tarea.dto';
 import { UpdateTareaDto } from './dto/update-tarea.dto';
 import { Usuario } from '../usuarios/entities/usuario.entity';
+import { GraphQLError } from 'graphql';
 
 @Injectable()
 export class TareasService {
@@ -17,13 +18,17 @@ export class TareasService {
   async create(createTarea: CreateTareaDto): Promise<Tarea> {
     //verificamos que el id que nos pasan sea valido
     if (!Types.ObjectId.isValid(createTarea.usuarioId)) {
-      throw new HttpException('ID inválido', HttpStatus.BAD_REQUEST);
+      throw new GraphQLError('ID inválido', {
+        extensions: { http: { status: 400 } },
+      });
     }
     // Buscar al usuario
     const usuario = await this.usuarioModel.findById(createTarea.usuarioId);
 
     if (!usuario)
-      throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
+      throw new GraphQLError('Usuario no encontrado', {
+        extensions: { http: { status: 404 } },
+      });
 
     // Crear la tarea
     const tarea = new this.tareaModel({
@@ -44,13 +49,18 @@ export class TareasService {
   }
   async findById(busqueda: BuscarTareaDto): Promise<Tarea> {
     if (!Types.ObjectId.isValid(busqueda._id)) {
-      throw new HttpException('ID inválido', 400);
+      throw new GraphQLError('ID inválido', {
+        extensions: { http: { status: 400 } },
+      });
     }
     const tarea = await this.tareaModel
       .findById(busqueda)
       .populate('usuario', 'email name ');
 
-    if (!tarea) throw new HttpException('Tarea no encontrada', 404);
+    if (!tarea)
+      throw new GraphQLError('Tarea no encontrada', {
+        extensions: { http: { status: 404 } },
+      });
 
     return tarea;
   }
@@ -58,11 +68,16 @@ export class TareasService {
   //Cuando se usa un dto como buscar tarea dto siempre es un objeto, nunca colocar busqueda._id
   async update(busqueda: BuscarTareaDto, updateTarea: UpdateTareaDto) {
     if (!Types.ObjectId.isValid(busqueda._id)) {
-      throw new HttpException('ID inválido', HttpStatus.BAD_REQUEST);
+      throw new GraphQLError('ID inválido', {
+        extensions: { http: { status: 400 } },
+      });
     }
     const findTarea = await this.tareaModel.findById(busqueda).exec();
 
-    if (!findTarea) throw new HttpException('Tarea no encontrada', 404);
+    if (!findTarea)
+      throw new GraphQLError('Tarea no encontrada', {
+        extensions: { http: { status: 404 } },
+      });
 
     return this.tareaModel.findByIdAndUpdate(busqueda, updateTarea, {
       new: true,
@@ -72,10 +87,15 @@ export class TareasService {
   //: Promise<Tarea> me da error talves sea que al eliminar ya no se puede leer los datos la verdad no se
   async delete(busqueda: BuscarTareaDto) {
     if (!Types.ObjectId.isValid(busqueda._id)) {
-      throw new HttpException('ID inválido', 400);
+      throw new GraphQLError('ID inválido', {
+        extensions: { http: { status: 400 } },
+      });
     }
     const data = await this.tareaModel.findByIdAndDelete(busqueda).exec();
-    if (!data) throw new HttpException('Tarea no encontrada', 404);
+    if (!data)
+      throw new GraphQLError('Tarea no encontrada', {
+        extensions: { http: { status: 404 } },
+      });
     return 'Tarea eliminada exitosamente';
   }
 }
